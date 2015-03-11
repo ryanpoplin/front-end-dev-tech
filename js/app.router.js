5c862a4f2e0707f526cbba1539356454e89147bf
Byrdann Fox <mobileupstate@gmail.com>
2015-03-11 18:41:14 -0400

    Updates...

--- js/app.router.js
+++ js/app.router.js
@@ -4,14 +4,28 @@
 
 	var coreApp = angular.module('coreApp', ['ngRoute']);
 
+    coreApp.controller('NavController', function ($scope) {
+
+        $scope.items = [{
+            href: '#/palindrome',
+            title: 'Palindrome'
+        }, {
+            href: '#/ex2',
+            title: 'Ex 2'
+        }];
+
+    });
+
 	coreApp.config(function ($routeProvider) {
 
   		$routeProvider
-    		.when('/ex1', {
+    		.when('/palindrome', {
+                controller: 'NavController',
     	    	templateUrl: 'partials/test.partial.one.html'
     	  	}).when('/ex2', {
+                controller: 'NavController',
     	  		templateUrl: 'partials/test.partial.two.html'
-    		}).otherwise({ redirectTo: '/ex1' });
+    		}).otherwise({ redirectTo: '/palindrome' });
 		});
 
 }());
