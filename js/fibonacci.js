7d9c1084656d0c0e980ed37059da9359fe81cc85
Byrdann Fox <mobileupstate@gmail.com>
2015-03-12 08:42:23 -0400

    Updates...

---
+++ js/fibonacci.js
@@ -0,0 +1,19 @@
+(function () {
+
+	'use strict';
+
+	var fibonacci = [];
+
+	fibonacci[0] = 0;
+	fibonacci[1] = 1;
+	fibonacci[2] = 2;
+
+	for (var i = 3; i < 10; i += 1) {
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+	}
+
+	for (var j = 1; j < fibonacci.length; j += 1) {
+		console.log(fibonacci[j]);
+	}
+
+}());
